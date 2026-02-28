7599a740758f8fc5e100c3bd319d82700fbb3cda
Kevin MacDermid <kevin.macdermid@gmail.com>
2026-02-27 21:14:01 -0400

    Attempt fix for volume low when restarting

--- run_reachy_supervisor.py
+++ run_reachy_supervisor.py
@@ -74,6 +74,16 @@ def main() -> int:
             if reset_code != 0:
                 print(f"reset_internal_hub.sh failed with exit code {reset_code}", file=sys.stderr)
                 return reset_code
+
+            # Set the volume level after restarting
+            subprocess.run([
+                "amixer",
+                "set",
+                "-M",
+                "PCM,1",
+                "85%"
+            ], check=True)
+
             print("Waiting before restarting daemon")
             sleep(20)
             continue
